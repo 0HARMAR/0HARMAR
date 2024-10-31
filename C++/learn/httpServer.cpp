f87f88e5ad81c9ab0d601283f732e15895254dc0
hemingyang_ClionGit <ttai33638@gamil.com>
2024-10-31 21:34:26 +0800

    new

---
+++ C++/learn/httpServer.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <boost/asio.hpp>
+
+using namespace std;
+using boost::asio::ip::tcp;
+
+void session(tcp::socket sock) {
+    try {
+        char data[1024];
+        while (true) {
+            boost::system::error_code error;
+            size_t length = sock.read_some(boost::asio::buffer(data), error);
+            if (error == boost::asio::error::eof) break; // Connection closed cleanly by peer
+            else if (error) throw boost::system::system_error(error); // Some other error
+
+            string response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!";
+            boost::asio::write(sock, boost::asio::buffer(response));
+        }
+    } catch (exception& e) {
+        cerr << "Exception in thread: " << e.what() << endl;
+    }
+}
+
+int main() {
+    try {
+        boost::asio::io_context io_context;
+        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 8080));
+
+        while (true) {
+            tcp::socket sock(io_context);
+            acceptor.accept(sock);
+            thread(session, move(sock)).detach();
+        }
+    } catch (exception& e) {
+        cerr << "Exception: " << e.what() << endl;
+    }
+    return 0;
+}
