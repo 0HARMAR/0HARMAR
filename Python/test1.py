a35f8c0c8076e3c8e47a4959a1d6a556f956864d
hemingyang_ClionGit <ttai33638@gamil.com>
2025-01-02 21:21:33 +0800

    new

--- Python/test1.py
+++ Python/test1.py
@@ -1,19 +1,3 @@
-import random
-
-"""it's seed test
-"""
-
-
-# 设置随机数种子
-random.seed(12)
-
-# 生成一系列随机数
-print(random.random())  # 0.6394267984578837
-print(random.randint(1, 10))  # 2
-print(random.choice(['apple', 'banana', 'cherry']))  # 'banana'
-
-# 重新设置种子，生成的随机数序列将与之前的相同
-random.seed(12)
-print(random.random())  # 0.6394267984578837
-print(random.randint(1, 10))  # 2
-print(random.choice(['apple', 'banana', 'cherry']))  # 'banana'
+for i in range(1,5):
+    for i in range(1,i+1):
+        print(i)
