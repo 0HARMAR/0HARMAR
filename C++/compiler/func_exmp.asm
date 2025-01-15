b45a87ee6266bd8674a951f171a75f04fb141fa1
hemingyang_ClionGit <ttai33638@gamil.com>
2025-01-15 21:51:50 +0800

    new

--- C++/compiler/func_exmp.asm
+++ C++/compiler/func_exmp.asm
@@ -1,14 +1,20 @@
+.section .data
+.
+
+
 .section .text
 .global _start
-_start:
-    movq $5 , %rdi         #第一个参数
-    movq $10 , %rsi        #第二个参数
+_start: , %rsi        #第二个参数
     call add
     movq $60, %rax        # syscall: exit
-    xor %edi, %rdi        # exit code 0
+    xor %rdi, %rdi        # exit code 0
     syscall
 
 add:
     addq %rdi , %rsi
     movq %rsi , %rax
     ret
+    movq $5 , %rdi         #第一个参数
+    movq $10
+
+print_int64:
